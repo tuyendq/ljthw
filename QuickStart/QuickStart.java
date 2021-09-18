ba023c2912a72a3dda905c99dd2781149d862d38
Tuyen Dang <tuyendq@gmail.com>
2021-09-18 21:56:10 +0700

    Edit .vscode: java.project.sourcePaths

--- QuickStart/QuickStart.java
+++ QuickStart/QuickStart.java
@@ -1,5 +1,6 @@
 class QuickStart {
   public static void main(String[] args) {
-    System.out.println("Get up and running with Java!");
+    // System.out.println("Get up and running with Java!");
+    System.out.println();("Get up and running with Java!");
   }
 }
