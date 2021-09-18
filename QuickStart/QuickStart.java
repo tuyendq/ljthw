c24f4fdee9270e1b11afd328b4ab3af031c97f19
Tuyen Dang <tuyendq@gmail.com>
2021-09-18 21:57:09 +0700

    Fix comment QuickStart

--- QuickStart/QuickStart.java
+++ QuickStart/QuickStart.java
@@ -1,6 +1,5 @@
 class QuickStart {
   public static void main(String[] args) {
-    // System.out.println("Get up and running with Java!");
-    System.out.println();("Get up and running with Java!");
+    System.out.println("Get up and running with Java!");
   }
 }
